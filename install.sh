4457c2d2baa51f912c24c3feec6a275f35e0599d
bender <sipio-africanus@proton.me>
2026-09-14 02:21:47 +0200

    Create install.sh for dependency installation
    Add installation script for dependencies including libcurl and tor.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+RESET='\033[0m'
+GRAS='\033[1m'
+
+
+printf "${VERT}${GRAS}instalation des dépendances :${RESET}\n\n"
+apt install libcurl4-openssl-dev && apt install tor
+printf "\n"
