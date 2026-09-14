403b58e31b0433032c1f1da7eca0049166fbd333
bender <sipio-africanus@proton.me>
2026-09-14 13:30:15 +0200

    Refactor SSL and HTTPS verification functions
    Refactor SSL and HTTPS verification functions to throw exceptions on errors and improve error handling.

--- src/lib-tor.h
+++ src/lib-tor.h
@@ -2,37 +2,123 @@
 #include <curl/easy.h>
 #include <iostream>
 #include <string>
-using namespace std;
+#include <stdexcept>
+
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::runtime_error;
 
 
 
 
-bool ssl_verification(string url){
+inline bool ssl_verification(string url){
 
     CURL* curl = curl_easy_init();
 
     if (curl == nullptr){
-        curl_easy_cleanup(curl);
+        throw  runtime_error("CURL err : curl == nullptr");
         return false;
     }
 
 
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_PROXY, "socks5h://127.0.0.1:9050");
+
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+
+    // Active la récupération des informations du certificat
+    curl_easy_setopt(curl, CURLOPT_CERTINFO, 1L);
+
     curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);  // HEAD request seulement
+
+    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 90L);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 120L);
+
+    CURLcode res_perform = curl_easy_perform(curl);
+
+    if (res_perform != CURLE_OK){
+
+        curl_easy_cleanup(curl);
+        return false;
+    }   
+
+    char* scheme = nullptr;
     
-    CURLcode res = curl_easy_perform(curl);
+    struct curl_certinfo* certinfo = nullptr;
+
+    if (curl_easy_getinfo(curl, CURLINFO_CERTINFO, &certinfo) == CURLE_OK &&
+        certinfo != nullptr &&
+        certinfo->num_of_certs > 0)
+    {
+        curl_easy_cleanup(curl);
+        return true;
+    }
 
     curl_easy_cleanup(curl);
-    return res == CURLE_OK;
+    return false;
+
+}
+
+
+
+inline bool https_verification(string url){
+
+    CURL* curl = curl_easy_init();
+
+    if (curl == nullptr){
+        return false;
+    }
+
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_PROXY, "socks5h://127.0.0.1:9050");
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+
+    // Active la récupération des informations du certificat
+    curl_easy_setopt(curl, CURLOPT_CERTINFO, 1L);
+
+    curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);  // HEAD request seulement
+
+    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 90L);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 120L);
+
+    CURLcode res_perform = curl_easy_perform(curl);
+
+    if (res_perform != CURLE_OK){
+
+        curl_easy_cleanup(curl);
+        return false;
+    }   
+
+    char* scheme = nullptr;
+    
+    CURLcode res = curl_easy_getinfo(curl, CURLINFO_SCHEME, &scheme);
+
+    if (res == CURLE_OK && scheme != nullptr){
+
+        if (std::string(scheme) == "HTTPS" || std::string(scheme) == "https"){
+
+            curl_easy_cleanup(curl);
+            return true;
+            
+        }
+
+    }
+
+    curl_easy_cleanup(curl);
+    return false;
 
 }
 
 
 
 //fonction callback
-static size_t manage_data(char* data, size_t size, size_t nmemb, void* userdata){
+static inline size_t manage_data(char* data, size_t size, size_t nmemb, void* userdata){
 
     //permet de remplire la variable de sortie avec le contenut de retour
     string* resultat = static_cast<string*>(userdata);
@@ -43,18 +129,15 @@ static size_t manage_data(char* data, size_t size, size_t nmemb, void* userdata)
 
 
 
-string tor_curl(const string& url, string user_agent, bool redirection){
+inline string tor_curl(const string& url, string user_agent, bool redirection){
 
     //initialisation de curl
     CURL* curl = curl_easy_init();
 
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
-
 
     if (curl == nullptr){
         curl_easy_cleanup(curl);
-        return "err : curl = nullptr";
+        throw runtime_error(std::string("CURL error: \033[31m curl == nullptr \033[0m"));
     }
 
 
@@ -90,11 +173,12 @@ string tor_curl(const string& url, string user_agent, bool redirection){
     CURLcode res = curl_easy_perform(curl);
 
     if (res != CURLE_OK){
-        cerr << "code d'erreur : " << curl_easy_strerror(res) << endl;
+         throw std::runtime_error(std::string("CURL error: ") + curl_easy_strerror(res));
         curl_easy_cleanup(curl);
         return "err";
     }
 
     curl_easy_cleanup(curl);
+
     return resultat;
 }
